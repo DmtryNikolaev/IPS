e08cf843b4505665b122a019dd389531754007f8
DimonNikolaev <dmitriy.nikolaev1337@gmail.com>
2022-06-02 20:35:13 +0500

    Контроллер

---
+++ app/Http/Controllers/RecordController.php
@@ -0,0 +1,110 @@
+<?php
+
+namespace App\Http\Controllers;
+
+use App\Models\Record;
+use Illuminate\Http\Request;
+
+
+class RecordController extends Controller
+{
+    /**
+     * Display a listing of the resource.
+     *
+     * @return \Illuminate\Http\Response
+     */
+    public function index()
+    {
+        $records = Record::all();
+        return view('record.index', compact('records'));
+    }
+
+    /**
+     * Show the form for creating a new resource.
+     *
+     * @return \Illuminate\Http\Response
+     */
+    public function create()
+    {
+        $record = new Record();
+        return view('record.create', compact('record'));
+    }
+
+    /**
+     * Store a newly created resource in storage.
+     *
+     * @param  \Illuminate\Http\Request  $request
+     * @return \Illuminate\Http\Response
+     */
+    public function store(Request $request)
+    {
+        $data = $this->validate($request, [
+            'date' => 'required',
+            'value' => 'required'
+        ]);
+
+        $record = new Record();
+        $record->fill($data);
+        $record->save();
+
+        return redirect()
+            ->route('records.index');
+    }
+
+    /**
+     * Display the specified resource.
+     *
+     * @param  \App\Models\Record  $record
+     * @return \Illuminate\Http\Response
+     */
+    public function show(Record $record)
+    {
+        return view('record.show', compact('record'));
+    }
+
+    /**
+     * Show the form for editing the specified resource.
+     *
+     * @param  \App\Models\Record  $record
+     * @return \Illuminate\Http\Response
+     */
+    public function edit(Record $record)
+    {
+        return view('record.edit', compact('record'));
+    }
+
+    /**
+     * Update the specified resource in storage.
+     *
+     * @param  \Illuminate\Http\Request  $request
+     * @param  \App\Models\Record  $record
+     * @return \Illuminate\Http\Response
+     */
+    public function update(Request $request, Record $record)
+    {
+        $data = $this->validate($request, [
+            'date' => 'required',
+            'value' => 'required'
+        ]);
+
+        $record->fill($data);
+        $record->save();
+
+        return redirect()
+            ->route('records.index');
+    }
+
+    /**
+     * Remove the specified resource from storage.
+     *
+     * @param  \App\Models\Record  $record
+     * @return \Illuminate\Http\Response
+     */
+    public function destroy(Record $record)
+    {
+        $record->delete();
+
+        return redirect()
+            ->route('records.index');
+    }
+}
