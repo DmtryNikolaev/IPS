546a5ac6d28f4d27b3db3673d6758a40fd210778
DimonNikolaev <dmitriy.nikolaev1337@gmail.com>
2022-06-02 20:36:18 +0500

    Список записей

---
+++ resources/views/record/index.blade.php
@@ -0,0 +1,15 @@
+@extends('layouts.app')
+
+@section('content')
+<h3>Список записей</h3>
+@foreach ($records as $record)
+<div class="border border-secondary mt-2 p-2 rounded-3">
+    <b>{{$record->date}}</b>
+    <p>{{$record->value}}</p>
+    {{ Form::open(['route' => ['records.destroy', $record], 'method' => 'delete']) }}
+    <button type="submit">Delete</button>
+    {{ Form::close() }}
+    <a href="{{ route('records.edit', $record) }}">Edit</a>
+</div>
+@endforeach
+@endsection
